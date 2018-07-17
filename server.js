1d5014a2c542fa434eaa90638072f0d1c46e2dd6
drdlx <drdlx@users.noreply.github.com>
2018-07-17 17:10:41 +0600

    production build

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
 const app = require('./app');
 const port = process.env.PORT;
+const http = require('http');
+const https = require('https');
 
-const server = app.listen(port, function() {
+const options = {
+  key: fs.readFileSync('./ssl/privatekey.pem'),
+  cert: fs.readFileSync('./ssl/certificate.pem'),
+};
+
+const server = https.createServer(options, app).listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
