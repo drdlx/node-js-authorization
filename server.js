baaa44854c2ac3c8b4c9891488c2d788d0b993cc
drdlx <drdlx@users.noreply.github.com>
2018-07-17 19:05:19 +0600

    additional changes

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 const app = require('./app');
-const port = process.env.PORT;
+const http_port = process.env.PORT;
 const http = require('http');
+
 const https = require('https');
+const https_port = process.env.PORT_HTTPS;
 
 const options = {
-  key: fs.readFileSync('./ssl/privatekey.pem'),
-  cert: fs.readFileSync('./ssl/certificate.pem'),
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
 };
 
-const server = https.createServer(options, app).listen(port, function() {
-  console.log('Express server listening on port ' + port);
+const server = https.createServer(options, app).listen(http_port, function() {
+  console.log('Express server listening on port ' + https_port);
 });
+
+http.createServer(function(req, res) {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'].replace(http_port, https_port) + req.url });
+  console.log("http request, will go to >> ");
+  console.log("https://" + req.headers['host'].replace(http_port,https_port) + req.url );
+  res.end();
+
+}).listen(http_port);
